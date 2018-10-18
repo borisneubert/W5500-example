2f3b7b0b24e1febfa6bb1deb7a80bb5b0e1fcbc3
Dr. Boris Neubert <omega@online.de>
2018-10-18 19:58:58 +0200

    minimal version to test successful compilation

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 //#include <Arduino.h>
 #include "ESP8266WiFi.h"
 #include <w5500-lwIP.h>
-void setup() {}
-void loop() {}
+
+#define CSPIN 15
+
+Wiznet5500lwIP ether(CSPIN);
+
+void setup() { ether.begin(); }
+void loop() { ether.loop(); }
