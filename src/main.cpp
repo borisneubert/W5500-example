38548509c670244f71d4ca5e1748c6cce79fb686
Dr. Boris Neubert <omega@online.de>
2018-10-19 20:01:02 +0200

    minimum code

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,28 @@
-//#include <Arduino.h>
+#include <Arduino.h>
 #include "ESP8266WiFi.h"
 #include <w5500-lwIP.h>
 
-#define CSPIN 15
+// Arduino Pin 4 = Wemos Pin D2
+#define CSPIN 4
 
 Wiznet5500lwIP ether(CSPIN);
 
-void setup() { ether.begin(); }
+byte mac[] = {0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02};
+
+void setup() {
+
+  Serial.begin(115200);
+  Serial.println("");
+  Serial.println("start");
+
+  int present = ether.begin(mac);
+  Serial.println("foo");
+  Serial.println("present= " + String(present, HEX));
+
+  while (!ether.connected()) {
+    Serial.print(".");
+    delay(1000);
+  }
+}
+
 void loop() { ether.loop(); }
