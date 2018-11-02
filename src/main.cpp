8d60b6619e06075ff0a20c95e0c2d866e322da82
Dr. Boris Neubert <omega@online.de>
2018-11-02 22:27:00 +0100

    fully working example with asynchronous web server and ntp client

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,99 @@
 #include <Arduino.h>
+
+// WiFi
 #include "ESP8266WiFi.h"
+const char *ssid = "ssid";
+const char *password = "password";
+
+// W5500,  Arduino Pin 4 = Wemos Pin D2
 #include <w5500-lwIP.h>
-
-// Arduino Pin 4 = Wemos Pin D2
 #define CSPIN 4
+Wiznet5500lwIP eth(CSPIN);
+byte mac[] = {0x00, 0x08, 0xDC, 0x12, 0x34, 0x56};
 
-Wiznet5500lwIP ether(CSPIN);
+// asynchronous web server
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+AsyncWebServer server(80);
 
-byte mac[] = {0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02};
-
+// ntp client
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+// #define NTPSERVER "0.ubuntu.pool.ntp.org"
+#define NTPSERVER "192.53.103.108"
+WiFiUDP ntpUDP;
+NTPClient ntpClient(ntpUDP, NTPSERVER, 3600, 20000);
+unsigned long long t = 0, t0 = 0;
+//
+// setup
+//
 void setup() {
 
+  // starting example
   Serial.begin(115200);
   Serial.println("");
-  Serial.println("start");
+  delay(1000);
+  Serial.println("starting example...");
 
-  ether.setDefault();
-  //int present = ether.begin(mac);
-  int present = ether.begin();
-  Serial.println("present= " + String(present, HEX));
+  // starting wifi
+  Serial.println("starting wifi...");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("wifi connection failed");
+    return;
+  } else {
+    Serial.print("wifi ip address: ");
+    Serial.println(WiFi.localIP());
+    Serial.print("wifi hostname: ");
+    Serial.println(WiFi.hostname());
+  }
 
-  while (!ether.connected()) {
+  // starting ethernet
+  Serial.println("starting ethernet...");
+  eth.setDefault(); // use ethernet for default route
+  int present = eth.begin();// eth.begin(mac);
+  // Serial.println("present= " + String(present, HEX));
+  if (!present) {
+    Serial.println("no W5500 present");
+    return;
+  }
+  Serial.print("connecting ethernet");
+  while (!eth.connected()) {
+    eth.loop();
     Serial.print(".");
     delay(1000);
   }
+  Serial.println();
+  Serial.print("ethernet ip address: ");
+  Serial.println(eth.localIP());
+
+  // starting web server
+  Serial.println("starting web server...");
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(200, "text/plain", "Hello, world");
+  });
+  server.begin();
+
+  // starting ntp client
+  Serial.println("starting ntp client...");
+  ntpClient.begin();
+
+  // ready
+  Serial.println("setup complete");
 }
 
-void loop() { ether.loop(); }
+void loop() {
+
+  eth.loop();
+
+  ntpClient.update();
+
+  t = ntpClient.getEpochTime();
+
+  if (t != t0) {
+    Serial.println(ntpClient.getFormattedTime());
+    t0 = t;
+  }
+
+}
